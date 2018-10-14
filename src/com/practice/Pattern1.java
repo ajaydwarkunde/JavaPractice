79dfa380d1beb57bf917a14f072e1adf138a1bb8
ajaydwarkunde <ajaydwarkunde365@gmail.com>
2018-10-14 20:17:32 +0530

    Initial commit for Pattern1

---
+++ src/com/practice/Pattern1.java
@@ -0,0 +1,23 @@
+package com.practice;
+
+/**
+ * @author HP
+ * 
+ * Write a program to print following pattern
+ * 1
+ * 22
+ * 333
+ * 4444
+ *
+ */
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+	}
+}
